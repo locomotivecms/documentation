067ad2cccb6ac4be9c5b4005e890bcb718448622
did <didier.lafforgue@gmail.com>
2012-02-14 00:49:37 -0800

    initialization

---
+++ config/compass.rb
@@ -0,0 +1,10 @@
+# Please, be careful when editing these settings, it may break the theme assets API push
+project_path      = '/Users/didier/Documents/LocomotiveCMS/documentation/public'
+http_path         = '/'
+css_dir           = '../tmp/stylesheets'
+sass_dir          = 'stylesheets'
+images_dir        = 'images'
+javascripts_dir   = 'javascripts'
+project_type      = :stand_alone
+output_style      = :nested
+line_comments     = false

27bfbec856b072ec5ec170aebf47f19e50c2f00b
Gabriel Nau <gabriel.nau@gmail.com>
2012-11-25 16:39:49 +0100

    Put every doc snippet in a sub page

---
+++ TODO.md
@@ -0,0 +1,18 @@
+## a valider
+
+- dans un enfant d'object, verifier que le sous menu object a la classe active
+
+
+## questions
+
+- faq sur le site principal ou sur celui ci ?
+- quick links footer : on les gardes ? ils redirigeront sur le site principal
+- 
+
+
+undocumented:
+
+  - filters:
+    - strip_html
+
+    == test remote branches
--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,23 +1,26 @@
 $(function() {
-  $("ul.tabs").tabs("#container-sidebar > section");
+  //$("ul.tabs").tabs("#container-sidebar > section");
   
+  //$('.dropdown-toggle').dropdown()
+
   //$("ul.dropdown").tabs();
   
-  $("ul.tabs a").click(function(){
-    $("ul.tabs a").removeClass("active");
-    $(this).toggleClass("active");
+  $("ul.dropdown a.dropdown-toggle").click(function(){
+    $("ul.dropdown .dropdown-menu").removeClass("expanded");
+    $(this).next().toggleClass("expanded");
+    return false
   });
 
-  $("ul.dropdown a").click(function(){
-    $(this).next().toggleClass("block");
-    return false;
-  });
+  // $("ul.dropdown a").click(function(){
+  //   $(this).next().toggleClass("block");
+  //   return false;
+  // });
 
-  $("ul.dropdown ul li a").click(function(){
-    tab_id = "#"+$(this).attr('id');
-    $("ul.dropdown").tabs("#container-sidebar > section",'select', tab_id);
-    return false;
-  });
+  // $("ul.dropdown ul li a").click(function(){
+  //   tab_id = "#"+$(this).attr('id');
+  //   $("ul.dropdown").tabs("#container-sidebar > section",'select', tab_id);
+  //   return false;
+  // });
 
   
   

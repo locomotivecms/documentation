6e5a700e092f48bee67ca1b0bb15cc0272bf3fb2
Gabriel Nau <gabriel.nau@gmail.com>
2012-11-25 18:31:26 +0100

    Update with tags section

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,28 +1,12 @@
 $(function() {
-  //$("ul.tabs").tabs("#container-sidebar > section");
-  
-  //$('.dropdown-toggle').dropdown()
 
-  //$("ul.dropdown").tabs();
-  
   $("ul.dropdown a.dropdown-toggle").click(function(){
     $("ul.dropdown .dropdown-menu").removeClass("expanded");
     $(this).next().toggleClass("expanded");
     return false
   });
 
-  // $("ul.dropdown a").click(function(){
-  //   $(this).next().toggleClass("block");
-  //   return false;
-  // });
-
-  // $("ul.dropdown ul li a").click(function(){
-  //   tab_id = "#"+$(this).attr('id');
-  //   $("ul.dropdown").tabs("#container-sidebar > section",'select', tab_id);
-  //   return false;
-  // });
-
-  
+    
   
 
   // mobile 

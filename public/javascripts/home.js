0ddaf610c22bb0f729f17f7b81b2255263727dfc
Jérémie Loosfelt <jloosfelt@gmail.com>
2013-02-18 13:25:31 +0100

    Added Guides menu

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -4,7 +4,7 @@ $(function() {
   $(".dropdown-menu").each(function(){
     current_section = $(this);
     current_section.find("a").each(function(){
-      console.log($(this).attr("href").indexOf(path))
+      // console.log($(this).attr("href").indexOf(path))
       if ($(this).attr("href").indexOf(path) != -1){
         current_section.addClass("expanded");
         $(this).addClass("active");
@@ -27,4 +27,8 @@ $(function() {
     $(this).toggleClass('expanded');
   });
   
+  $('#main-nav li.guides > a').on('click', function(e){
+    e.preventDefault();
+    $('#guides').toggle();
+  });
 });

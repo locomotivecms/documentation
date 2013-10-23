997cfe09125f5693b677d763c098be460621622a
did <didier.lafforgue@gmail.com>
2013-10-23 08:45:30 -0400

    first push to the hosting platform

---
+++ public/javascripts/home.js
@@ -0,0 +1,20 @@
+$(function() {
+  // $("ul.tabs").tabs(".panes > section");
+
+  $('.home-row h1').each(function(index) {
+    $el = $(this)
+    $el.html('<span>' + $el.text() + '</span>')
+  })
+
+  $('#home-cred h1').each(function(index) {
+    $el = $(this)
+    $el.html('<span>' + $el.text() + '</span>')
+  })
+
+  // mobile
+  $('#nav-toggle').on('click',function(e){
+    $('#main-nav').toggleClass('expanded');
+    $(this).toggleClass('expanded');
+  });
+
+});
